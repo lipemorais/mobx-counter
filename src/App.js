626df3f2c1c180143dc1bbdce5f387999eaf0da0
Felipe Morais <fmorais@thoughtworks.com>
2018-12-16 18:03:50 -0200

    counter with mobx managing state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
 class App extends Component {
+  myState = observable({ counter: 0 });
+
+  increment = () => {
+    this.myState.counter++;
+  };
+
+  decrement = () => {
+    this.myState.counter--;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <span>{this.myState.counter}</span>
+        <div>
+          <button onClick={this.decrement}>decrementar</button>
+          <button onClick={this.increment}>incrementar</button>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default observer(App);
